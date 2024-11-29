574d593e9c82776fce1be9c808c3644cb6f0fbb7
Brindha Manickavasakan <72887609+Brindha-m@users.noreply.github.com>
2024-11-29 06:19:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ import torch
 import intel_extension_for_pytorch as ipex
 import openvino.runtime as ov
 
-ov_model = ov.Core().read_model("yolov8x_openvino_model/")
+# ov_model = ov.Core().read_model("yolov8x_openvino_model/")
 
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
@@ -216,7 +216,11 @@ model_select = "yolov8xcdark.pt"
 model = YOLO(model_select,'conf=0.45')  # Model initialization
 model_seg = "yolov8xcdark-seg.pt"
 model1 = YOLO(model_seg)  # Model initialization
-      
+
+# Export the model
+model_select.export(format="openvino")  # creates 'yolov8n_openvino_model/'
+# Load the exported OpenVINO model
+ov_model = YOLO("yolov8n_openvino_model/")
 
 source = ("Image Detection📸", "Video Detections📽️", "Live Camera Detection🤳🏻","RTSP","MOBILE CAM")
 source_index = st.sidebar.selectbox("Select Input type", range(
